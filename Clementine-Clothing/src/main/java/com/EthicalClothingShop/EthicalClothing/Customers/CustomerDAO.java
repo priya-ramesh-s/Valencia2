5827afef1646f4428bc7a4ac3c92dd661e8ad699
priya-ramesh-s <88005090+priya-ramesh-s@users.noreply.github.com>
2021-10-01 12:03:49 +0100

    delete

--- Clementine-Clothing/src/main/java/com/EthicalClothingShop/EthicalClothing/Customers/CustomerDAO.java
+++
@@ -1,7 +0,0 @@
-package com.EthicalClothingShop.EthicalClothing.Customers;
-
-import com.EthicalClothingShop.EthicalClothing.ClothingLine.ClothingItem;
-
-public interface CustomerDAO {
-   // public Customer getCustomerAccountInfo(String customerEmail);
-}
