68d862e0ef3ad1161cd9e12244c1a92d400e6785
priya-ramesh-s <88005090+priya-ramesh-s@users.noreply.github.com>
2021-10-04 13:32:18 +0100

    commit 1

--- Clementine-Clothing/src/main/java/com/EthicalClothingShop/Configurations/SpringGlobalConfig.java
+++ Clementine-Clothing/src/main/java/com/EthicalClothingShop/Configurations/SpringGlobalConfig.java
@@ -1,6 +1,6 @@
 package com.EthicalClothingShop.Configurations;
 
-mport org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
